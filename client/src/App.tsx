0d050956eaefd889d710820dc049c61432280c3f
Charles-West-Africa <charlesuwem94@gmail.com>
2025-04-12 23:52:15 +0100

    Setup React with Vite, configure Tailwind and DaisyUI

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>React App</div>
+      <button className="btn btn-primary">test daisyui</button>
+    </>
+  );
+}
+
+export default App;
