9a7a90e7ef219335f3a1288fc4fc6b851b3e26eb
Charles-West-Africa <charlesuwem94@gmail.com>
2025-04-18 11:00:08 +0100

    Login has separate page, route created for it App file via React-Router-Dom

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col">
+          <div className="">
+            <h2 className="text-2xl mb-6 md:text-3xl">
+              <span className="text-base-content">Life</span>
+              <span className="text-primary">Care</span>
+            </h2>
+            <h1 className="text-base-content text-center text-4xl font-bold lg:text-5xl">
+              Welcome Back!
+            </h1>
+          </div>
+          <fieldset className="fieldset flex flex-col w-full max-w-sm shrink-0">
+            <input
+              type="email"
+              className="input mb-3 w-full"
+              placeholder="Email"
+            />
+            <input
+              type="password"
+              className="input mb-2 w-full"
+              placeholder="Password"
+            />
+            <div className="self-end">
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+            <button className="btn btn-primary mt-2">Login</button>
+            <div>
+              <p className="text-center text-[1rem] mt-4">
+                Don't have an account?{" "}
+                <span className="text-primary cursor-pointer">Sign up</span>
+              </p>
+            </div>
+          </fieldset>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
