aa6b091d6bfccd22ab83674a155d72bc555514a6
Charles-West-Africa <charlesuwem94@gmail.com>
2025-05-05 19:37:44 +0100

    Add a sample chart component to be customized later

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import DashboardHeader from "../components/DashboardHeader";
 import Stat from "../components/Stat";
 import Card from "../components/Card";
+import ScoreLineChart from "../components/Chart";
 
 const Dashboard: React.FC = () => {
   return (
@@ -13,17 +14,23 @@ const Dashboard: React.FC = () => {
         <Stat bg="carton" />
         <Stat bg="lilac" />
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 lg:grid-rows-2 gap-6 mt-5">
-        <Card>
-          <h2 className="card-title">Card Title</h2>
-          <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
-          </p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 lg:grid-rows-2 gap-6 mt-5">
+        {/* <div className="card card-border bg-base-100 shadow lg:col-span-2">
+          <div className="card-body">
+            <h2 className="card-title">Card Title</h2>
+            <p>
+              A card component has a figure, a body part, and inside body there
+              are title and actions parts. Last
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
           </div>
-        </Card>
+        </div> */}
+        <div className="shadow lg:col-span-2">
+          <ScoreLineChart />
+        </div>
+
         <Card>
           <h2 className="card-title">Card Title</h2>
           <p>
@@ -46,7 +53,7 @@ const Dashboard: React.FC = () => {
             </div>
           </div>
         </div>
-        <div className="card card-border bg-base-100 shadow lg:col-span-2">
+        <div className="card card-border bg-base-100 shadow lg:col-span-3">
           <div className="card-body">
             <h2 className="card-title">Card Title</h2>
             <p>
