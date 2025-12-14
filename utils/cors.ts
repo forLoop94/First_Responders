2372e1edde53820cdc7a6bc81ac5cb5d3d6fc3a1
Charles-West-Africa <charlesuwem94@gmail.com>
2025-12-15 00:44:57 +0100

    Add domain to CORS

--- utils/cors.ts
+++ utils/cors.ts
@@ -1,6 +1,10 @@
 export const useCors = () => {
   return {
-    origin: ["http://localhost:5173", "http://localhost:5175"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5175",
+      "http://localhost:5500",
+    ],
     credentials: true,
   };
 };
